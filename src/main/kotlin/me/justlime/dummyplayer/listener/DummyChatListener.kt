70d2700e6c6978f583992f8038ca9dea96c4a2d4
yashv <itsyashvardhan76@gmail.com>
2026-01-28 11:17:49 +0530

    added msg

--- src/main/kotlin/me/justlime/dummyplayer/listener/DummyChatListener.kt
+++ src/main/kotlin/me/justlime/dummyplayer/listener/DummyChatListener.kt
@@ -9,9 +9,9 @@ import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 
 object DummyChatListener {
-    private const val MESSAGE_LIMIT = 15
+    private const val MESSAGE_LIMIT = 20
 
-    // Stores the last 15 messages for each dummy (Key: DummyName, Messages)
+    // Stores the last 20 messages for each dummy (Key: DummyName, Messages)
     private val chatHistory = ConcurrentHashMap<String, MutableList<Message>>()
 
     @ApiStatus.Internal
@@ -26,7 +26,7 @@ object DummyChatListener {
             history.add(message)
         }
 
-        updateUIForViewers(targetPlayerName)
+        UIManager.updateUIForViewers(targetPlayerName)
     }
 
     /**
@@ -36,26 +36,4 @@ object DummyChatListener {
         return chatHistory[dummyName] ?: emptyList()
     }
 
-    private fun updateUIForViewers(dummyName: String) {
-        val viewers = DummySelectorService.getPlayersWatchingDummy(dummyName.uppercase())
-        println("$viewers $dummyName")
-        for (viewerId in viewers) {
-            val uiPair = UIManager.playersDummyUI[viewerId] ?: continue
-            val page = uiPair.first
-            val template = uiPair.second
-            val newLog = getLogForUI(dummyName)
-            newLog.forEach {
-                val player = Universe.get().getPlayer(viewerId)
-                if (player == null) {
-                    println("PlayerNotFound")
-                    return@forEach
-                }
-                val rawText = it.ansiMessage
-                player.sendMessage(Message.raw("[$dummyName] $rawText"))
-            }
-            // Update the template variable
-            template.setVariable("chat_messages", newLog.joinToString { "<p>${it.ansiMessage}</p>" })
-            page.updatePage(false)
-        }
-    }
 }
--- src/main/kotlin/me/justlime/dummyplayer/ui/UIManager.kt
+++ src/main/kotlin/me/justlime/dummyplayer/ui/UIManager.kt
@@ -1,28 +1,27 @@
 package me.justlime.dummyplayer.ui
 
 import au.ellie.hyui.builders.DropdownBoxBuilder
-import au.ellie.hyui.builders.HyUIPage
 import au.ellie.hyui.builders.PageBuilder
 import au.ellie.hyui.html.TemplateProcessor
+import com.hypixel.hytale.protocol.packets.interface_.ChatMessage
 import com.hypixel.hytale.protocol.packets.interface_.CustomUIEventBindingType
+import com.hypixel.hytale.server.core.HytaleServer
 import com.hypixel.hytale.server.core.Message
 import com.hypixel.hytale.server.core.universe.PlayerRef
+import me.justlime.dummyplayer.listener.DummyChatListener
 import me.justlime.dummyplayer.service.DummyPlayerFactory
 import me.justlime.dummyplayer.service.DummySelectorService
-import java.util.*
-import java.util.concurrent.ConcurrentHashMap
+
 
 object UIManager {
+    data class ChatLine(val text: String)
 
-    val playersDummyUI = ConcurrentHashMap<UUID, Pair<HyUIPage, TemplateProcessor>>()
+    val messages: MutableList<ChatLine> = mutableListOf()
 
     fun open(playerRef: PlayerRef) {
         val dummyNames = DummyPlayerFactory.getDummyNames()
-
-        val template = TemplateProcessor().setVariable("chat_messages", "<p>No Messages Yet...</p>")
-
-        val pageBuilder = PageBuilder.pageForPlayer(playerRef)
-            .loadHtml("Pages/Menu.html")
+        val template = TemplateProcessor().setVariable("messages", messages)
+        val pageBuilder = PageBuilder.pageForPlayer(playerRef).loadHtml("Pages/Menu.html", template)
 
         pageBuilder.elements.forEach { elementBuilder ->
             if (elementBuilder.id == "dummy-list") {
@@ -32,14 +31,7 @@ object UIManager {
                 }
                 dropDown.addEventListener(CustomUIEventBindingType.ValueChanged) {
                     DummySelectorService.selectDummy(playerRef.uuid, it)
-                    playerRef.sendMessage(
-                        Message.raw(
-                            "Selected Dummy: ${
-                                DummySelectorService.getSelectedDummy(playerRef.uuid) ?: "NONE"
-                            }"
-                        )
-                    )
-                    playerRef.sendMessage(Message.raw(DummySelectorService.getSessions().toString()))
+                    updateUIForViewers(it)
                 }
                 dropDown.withValue(DummySelectorService.getSelectedDummy(playerRef.uuid) ?: "NONE")
             }
@@ -49,8 +41,36 @@ object UIManager {
             playerRef.sendMessage(Message.raw("Store is null"))
             return
         }
-        val page = pageBuilder.open(store).page.get()
-        playersDummyUI[playerRef.uuid] = Pair(page, template)
+        pageBuilder.addEventListener("chat-input", CustomUIEventBindingType.Validating) { _, context ->
+            context.getValue("chat-input", String::class.java).ifPresent { message ->
+                if (message.isNotBlank()) {
+                    sendChat(playerRef, message)
+                }
+            }
+        }
+        pageBuilder.open(store)
+    }
 
+
+    fun updateUIForViewers(dummyName: String) {
+        val newLog = DummyChatListener.getLogForUI(dummyName)
+        messages.clear()
+        messages.addAll(newLog.map { ChatLine(it.ansiMessage) })
+    }
+
+
+    private fun sendChat(playerRef: PlayerRef, input: String) {
+        if (input.isBlank()) return
+        when (input.first()) {
+            '.' -> playerRef.chat(input.removePrefix(".")) //TODO
+            '/' -> HytaleServer.get().commandManager.handleCommand(playerRef, input.removePrefix("/"))
+            else -> playerRef.chat(input)
+        }
+    }
+
+    private fun PlayerRef.chat(text: String) {
+        val packet = ChatMessage(text)
+        val handler = this.packetHandler
+        handler.handle(packet)
     }
 }
